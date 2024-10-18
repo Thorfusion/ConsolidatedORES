5e6cdfbe008d2473a637cffbd88499cc27e8d73a
maggi373 <40539743+maggi373@users.noreply.github.com>
2024-10-19 00:30:50 +0200

    first push

---
+++ src/main/java/com/thorfusion/terralizationcompat/consolidatedoresConfig.java
@@ -0,0 +1,27 @@
+package com.thorfusion.terralizationcompat;
+
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class consolidatedoresConfig {
+    public static boolean EnableQuartzCompat;
+    public static boolean EnableDiamondCompat;
+    public static boolean EnableCapes;
+    public static boolean EnablePoorOresCompat;
+
+    public static void init() {
+        Configuration config;
+        //this line either creates the file if it doesn't exist or opens it if it already exists.
+        config = new Configuration(new File("config/Terralization.cfg"));
+        config.load();//reads the contents of the file into the Configuration object.
+
+        //General
+        EnableCapes = config.get("General", "Capes", true).getBoolean();
+        try {
+            config.save();//saves the Configuration content into the file.
+        } catch (Exception e) {
+            System.out.print("Could't save changes to the config file\n" + e);
+        }
+    }
+}
